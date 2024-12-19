8b0a5632d93bd89153745046a736deff277ac35c
DSGrid23 <147314029+DSGrid23@users.noreply.github.com>
2024-12-19 21:57:06 +0700

    add Timer

--- cuda/common.hpp
+++ cuda/common.hpp
@@ -31,3 +31,40 @@
 #else
 #define BREAK {}
 #endif
+
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+        cudaEventSynchronize(start);
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
